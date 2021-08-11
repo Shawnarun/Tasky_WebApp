37cda4938e5f2424676240b309ceb747baa31bf9
S.Shawn Arunjith <shawna@sltc.edu.lk>
2021-08-11 21:11:06 +0530

    js last edit

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const taskContainer = document.querySelector(".task__container");
+let globalTaskData = [];
+const genarateHtml=(taskData)=>`<div id=${taskData.id} class="col-md-6 col-lg-4 my-4">
+<div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button class="btn btn-outline-info">
+      <i class="fal fa-pencil"></i>
+    </button>
+    <button class="btn btn-outline-danger">
+      <i class="far fa-trash-alt"></i>
+    </button>
+  </div>
+  <div class="card-body">
+    <img
+      src=${taskData.image}
+      alt="image"
+      class="card-img"
+    />
+    <h5 class="card-title mt-4">${taskData.title}</h5>
+    <p class="card-text">
+    ${taskData.description}
+    </p>
+    <span class="badge bg-primary">${taskData.type}</span>
+  </div>
+  <div class="card-footer">
+    <button class="btn btn-outline-primary">Open Task</button>
+  </div>
+</div>
+</div>`
+ 
+const insertintoDom=(content)=>    taskContainer.insertAdjacentHTML("beforeend", content);
 
 const addNewCard = () => {
     const taskData = {
@@ -9,47 +39,45 @@ const addNewCard = () => {
        description:document.getElementById("taskDescrption").value,
 
     };
-
-
-    const newCard = `<div id=${taskData.id} class="col-md-6 col-lg-4 my-4">
-    <div class="card">
-      <div class="card-header d-flex justify-content-end gap-2">
-        <button class="btn btn-outline-info">
-          <i class="fal fa-pencil"></i>
-        </button>
-        <button class="btn btn-outline-danger">
-          <i class="far fa-trash-alt"></i>
-        </button>
-      </div>
-      <div class="card-body">
-        <img
-          src=${taskData.image}
-          alt="image"
-          class="card-img"
-        />
-        <h5 class="card-title mt-4">${taskData.title}</h5>
-        <p class="card-text">
-        ${taskData.description}
-        </p>
-        <span class="badge bg-primary">${taskData.type}</span>
-      </div>
-      <div class="card-footer">
-        <button class="btn btn-outline-primary">Open Task</button>
-      </div>
-    </div>
-    </div>`
+    globalTaskData.push(taskData);
+    //update the local storage
+    localStorage.setItem("taskyCA",JSON.stringify({card :globalTaskData}));
 
 
 
-    taskContainer.insertAdjacentHTML("beforeend", newCard);
+    const newCard = genarateHtml(taskData);
+    insertintoDom(newCard);
 
 
- document.getElementById("taskTitle").value="";
+    document.getElementById("taskTitle").value="";
        document.getElementById("imageURL").value="";
        document.getElementById("taskType").value="";
        document.getElementById("taskDescrption").value="";
 
 
+return;
 };
 
+
+
+const loadExistingCards= () => {
+
+const getData = localStorage.getItem("taskyCA");
+
+if(!getData) return;
+
+const taskCards =JSON.parse(getData);
+globalTaskData=taskCards.card;
+globalTaskData.map((taskData)=>{
+  const newCard =genarateHtml(taskData);
+
+
+
+  insertintoDom(newCard);
+
+
+
+});
+return;
+};
 
